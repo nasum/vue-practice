9c03b1ddb151e757b19485186d0f07659605fe96
Masaya Nasu <tomato.wonder.life@gmail.com>
2015-10-20 01:06:10 +0900

    コンポーネントの練習

--- component-practice/index.js
+++ component-practice/index.js
@@ -3,10 +3,25 @@ $(document).ready(function(){
     template: '<div>component</div>'
   });
 
+  var MyComponentList = Vue.extend({
+    template: '<ul><li v-repeat="list" v-on="click: clickList">{{$value}}</li></ul>',
+    data: function(){
+      return {
+        list: 10
+      }
+    },
+    methods: {
+      clickList: function(e){
+        alert(e.targetVM.$index);
+      }
+    }
+  });
+
   var vue = new Vue({
     el: 'body',
     components: {
-      myComponent: MyComponent
+      myComponent: MyComponent,
+      myListComponent: MyComponentList
     }
   });
 });
